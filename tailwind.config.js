6e555b5c93e63b70f5a26bc13862a5a52cf6a79b
Chris <94694991+QCHR1581@users.noreply.github.com>
2022-03-20 11:22:56 +0100

    layout changes plus responsivenes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      'sm': '576px',
+      // => @media (min-width: 576px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 960px) { ... }
+
+      'lg': '1026px',
+      // => @media (min-width: 1440px) { ... }
+    },
     extend: {},
   },
   plugins: [],
