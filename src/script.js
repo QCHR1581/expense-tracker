4d8035ab857dfdd20217026657c93b54741d2244
Chris <94694991+QCHR1581@users.noreply.github.com>
2022-03-24 16:28:21 +0100

    added  modal when button clicked

---
+++ src/script.js
@@ -0,0 +1,15 @@
+const overlay = document.querySelector("#overlay");
+const modal = document.querySelector("#modal");
+const openModalButton = document.querySelector("#plus-button");
+
+openModalButton.addEventListener("click", displaylog = () => {
+    overlay.classList.remove("hidden");
+    modal.classList.remove("hidden");
+})
+
+
+
+
+
+
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      'sm': '576px',
+      // => @media (min-width: 576px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1025px',
+      // => @media (min-width: 1024px) { ... }
+    },
     extend: {},
   },
   plugins: [],
