40d3e20251c31d7a506059c8383931b9cc726db0
Chris <94694991+QCHR1581@users.noreply.github.com>
2022-03-25 12:23:29 +0100

    Merge branch 'main' of https://github.com/QCHR1581/expense-tracker

--- src/script.js
+++ src/script.js
@@ -1,10 +1,24 @@
 const overlay = document.querySelector("#overlay");
 const modal = document.querySelector("#modal");
 const openModalButton = document.querySelector("#plus-button");
+const closeModalButton = document.querySelector("#close-button");
+const audioOpen = new Audio();
+audioOpen.src = "./openClick.mp3";
+audioOpen.volume = 0.5;
+const audioClose = new Audio();
+audioClose.src = "./closeClick.mp3";
+audioClose.volume = 0.5;
 
-openModalButton.addEventListener("click", displaylog = () => {
-    overlay.classList.remove("hidden");
-    modal.classList.remove("hidden");
+openModalButton.addEventListener("click", removeHiddenClass = () => {
+    overlay.classList.remove("scale-0");
+    modal.classList.remove("scale-0");
+    audioOpen.play();
+})
+
+closeModalButton.addEventListener("click", closeWindow = () => {
+    overlay.classList.add("scale-0");
+    modal.classList.add("scale-0");
+    audioClose.play();
 })
 
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,10 @@ module.exports = {
       // => @media (min-width: 576px) { ... }
 
       'md': '768px',
-      // => @media (min-width: 960px) { ... }
+      // => @media (min-width: 768px) { ... }
 
       'lg': '1026px',
-      // => @media (min-width: 1440px) { ... }
+      // => @media (min-width: 1026px) { ... }
     },
     extend: {},
   },
