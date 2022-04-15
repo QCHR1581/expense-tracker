bc606c4d91f3d71fb3ecbe9b78d8ae3f6fe459a1
Chris <94694991+QCHR1581@users.noreply.github.com>
2022-04-15 19:30:23 +0200

    added sound effects and calculations

--- script.js
+++ script.js
@@ -5,27 +5,26 @@ const openModalButton = document.querySelector("#plus-button");
 const closeModalButton = document.querySelector("#close-button");
 
 // Get audio files
-// const audioOpen = new Audio();
-// audioOpen.src = "/src/openClick.mp3";
-// audioOpen.volume = 0.5;
-// const audioClose = new Audio();
-// audioClose.src = "/src/closeClick.mp3";
-// audioClose.volume = 0.5;
+const audioOpen = new Audio();
+audioOpen.src = "/src/openClick.mp3";
+audioOpen.volume = 0.5;
+const audioClose = new Audio();
+audioClose.src = "/src/closeClick.mp3";
+audioClose.volume = 0.5;
 
 // Opens modal with sound
 openModalButton.addEventListener("click", removeHiddenClass = () => {
     overlay.classList.remove("zero");
     modal.classList.remove("zero");
-    // audioOpen.play();
+    audioOpen.play();
 })
 
 // Closes modal with sound
 closeModalButton.addEventListener("click", closeWindow = () => {
     overlay.classList.add("zero");
     modal.classList.add("zero");
-    // audioClose.play();
+    audioClose.play();
 })
-
 
 
 // Gets necessary elements for creating ul 
@@ -48,12 +47,52 @@ getDate = () => {
     
 }
 
-getAmount = () => {
+displayStatements = () => {
     if (statements.options[statements.selectedIndex].text === "Income") {
        let currentValue = parseFloat(document.querySelector("#income").textContent) || 0;
+       console.log(currentValue);
        let newValue = parseFloat(amount.value);
-       document.querySelector("#income").textContent = currentValue + newValue; 
-    } 
+       console.log(newValue);
+
+       let sum = currentValue + newValue;
+       let formatSum = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(sum);
+       document.querySelector("#income").textContent = formatSum; 
+
+       const incomeAudio = new Audio;
+       incomeAudio.src = "/src/addedIncome.wav";
+       incomeAudio.volume = 0.5;
+       incomeAudio.play();
+       
+
+    } else if (statements.options[statements.selectedIndex].text === "Expense") {
+       let currentValue = parseFloat(document.querySelector("#expenses").textContent) || 0;
+       let newValue = parseFloat(amount.value);
+       let sum = currentValue + newValue;
+       let formatSum = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(sum);
+       document.querySelector("#expenses").textContent = formatSum; 
+
+       const expemseAudio = new Audio;
+       expemseAudio.src = "/src/addedExpense.mp3";
+       expemseAudio.volume = 0.5;
+       expemseAudio.play();
+    }
+
+displayBalance = () => {
+    
+
+    
+
+    let formatBalance = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(balance);
+    document.querySelector("#balance").textContent = formatBalance; 
+
+    if (balance < 0) {
+        balanceText = document.querySelector("#balance");
+        balanceText.classList.add("text-red-400");
+    }
+  
+
+}
+
 }
 
 // Gets Add-Button
@@ -64,22 +103,25 @@ addButton.addEventListener("click", submitInputs = () => {
 
     if (!date.value && !amount.value) {
         alert("Please enter date and amount!");
-        date.classList.add("border-2", "border-red-400");
-        amount.classList.add("border-2", "border-red-400");
+        date.classList.add("border-4", "border-red-400");
+        amount.classList.add("border-4", "border-red-400");
 
     } else if (!date.value) {
         alert("Please enter date!");
-        date.classList.add("border-2", "border-red-400");
+        date.classList.add("border-4", "border-red-400");
 
     } else if (!amount.value) {
         alert("Please enter amount!");
-        amount.classList.add("border-2", "border-red-400");
+        amount.classList.add("border-4", "border-red-400");
 
     } else {
         getStatement();
         getTypes();
         getDate();
-        getAmount();
+        displayStatements();
+        displayBalance();
+        date.classList.remove("border-4", "border-red-400");
+        amount.classList.remove("border-4", "border-red-400");
     } 
 })
 
